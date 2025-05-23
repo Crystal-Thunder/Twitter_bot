2fdcbab80d40884c4e7901adca5947ff3f837a2f
Jason Joseph <jasonjoseph381@gmail.com>
2025-05-23 23:35:50 +0100

    a bot that monitors your activities on x

---
+++ twitter.py
@@ -0,0 +1,167 @@
+import tweepy
+from Tkinter import *
+import tkinter as tk
+
+consumer_key = 'consumer key'
+consumer_secret = 'consumer secrets'
+access_token = 'access token'
+access_token_secret = 'access token secret'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+print(user.name)
+print(user.location)
+
+for follower in tweepy.Cursor(api.followers).items():
+    follower.follow()
+
+print("Followed everyone that is following " + user.name)
+
+root = tk.Tk()
+
+label1 = tk.Label(root, text="Search")
+E1 = tk.Entry(root, bd=5)
+
+label2 = tk.Label(root, text="Number of Tweets")
+E2 = tk.Entry(root, bd=5)
+
+label3 = tk.Label(root, text="Response")
+E3 = tk.Entry(root, bd=5)
+
+label4 = tk.Label(root, text="Reply?")
+E4 = tk.Entry(root, bd=5)
+
+label5 = tk.Label(root, text="Retweet?")
+E5 = tk.Entry(root, bd=5)
+
+label6 = tk.Label(root, text="Favorite?")
+E6 = tk.Entry(root, bd=5)
+
+label7 = tk.Label(root, text="Follow?")
+E7 = tk.Entry(root, bd=5)
+
+def getE1():
+    return E1.get()
+
+def getE2():
+    return E2.get()
+
+def getE3():
+    return E3.get()
+
+
+def getE4():
+    return E4.get()
+
+def getE5():
+    return E5.get()
+
+def getE6():
+    return E6.get()
+
+def getE7():
+    return E7.get()
+
+
+def mainFunction():
+    getE1()
+    search = getE1()
+    
+    getE2()
+    numberOfTweets = getE2()
+    numberOfTweets = int(numberOfTweets)
+    
+    getE3()
+    phrase = getE3()
+    
+    getE4()
+    reply = getE4()
+    
+    getE5()
+    retweet = getE5()
+    
+    getE6()
+    favorite = getE6()
+
+    getE7()
+    follow = getE7()
+
+    if reply == "yes":
+        for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+            try:
+                #Reply
+                print('\nTweet by: @' + tweet.user.screen_name)
+                print('ID: @' + str(tweet.user.id))
+                tweetId = tweet.user.id
+                username = tweet.user.screen_name
+                api.update_status("@" + username + " " + phrase, in_reply_to_status_id = tweetId)
+                print ("Replied with " + phrase)
+                
+            except tweepy.TweepError as e:
+                print(e.reason)
+
+            except StopIteration:
+                break
+
+
+    if retweet == "yes": 
+        for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+            try:
+                #Retweet
+                tweet.retweet()
+                print('Retweeted the tweet')   
+
+            except tweepy.TweepError as e:
+                print(e.reason)
+
+            except StopIteration:
+                break
+
+    if favorite == "yes": 
+        for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+            try:
+                #Favorite
+                tweet.favorite()
+                print('Favorited the tweet')   
+
+            except tweepy.TweepError as e:
+                print(e.reason)
+
+            except StopIteration:
+                break
+
+    if follow == "yes": 
+        for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+            try:
+                #Follow
+                tweet.user.follow()
+                print('Followed the user')
+                
+            except tweepy.TweepError as e:
+                print(e.reason)
+
+            except StopIteration:
+                break       
+            
+submit = tk.Button(root, text ="Submit", command = mainFunction)
+
+label1.pack()
+E1.pack()
+label2.pack()
+E2.pack()
+label3.pack()
+E3.pack()
+label4.pack()
+E4.pack()
+label5.pack()
+E5.pack()
+label6.pack()
+E6.pack()
+label7.pack()
+E7.pack()
+submit.pack(side=tk.BOTTOM)
+
+root.mainloop()
